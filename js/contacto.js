4558e054196905d79a8121b42846a9d1724e3379
elizabethcmcf <elizabethcm711@gmail.com>
2025-05-08 05:51:27 -0500

    v.07

--- js/contacto.js
+++ js/contacto.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded",function(){
-    document.addEventListener("submit",function(event){
-        event.preventDefault()
+    document.getElementById("validar").addEventListener("click",function(event){
         let nombre=document.getElementById("nombre").value
         let email=document.getElementById("email").value
         let tel=document.getElementById("tel").value
         let asunto=document.getElementById("asunto").value
         let msg=document.getElementById("msg").value
-        let mensaje=`Nombre: ${nombre}\nEmail: ${email}\nTeléfono: ${tel}\nAsunto: ${asunto}\nMensaje: ${msg}`  
-        let url=`https://api.whatsapp.com/send?phone=5491155555555&text=${encodeURIComponent(mensaje)}`
-        window.open(url,"_blank")
-
+        let mensaje=`Nombre: ${nombre}\nEmail: ${email}\nTeléfono: ${tel}\nAsunto: ${asunto}\nMensaje: ${msg}`;  
+        alert("Se enviará esta información a ALCAM:\n"+mensaje+".\n Si el mensaje se encuentra correcto haga clic en Aceptar y luego en gitel botón Enviar.")
         })
     })
