d9a5a17046e5d2f3115f7b9951d651541ec1dadb
elizabethcmcf <elizabethcm711@gmail.com>
2025-05-08 01:22:22 -0500

    v.02

--- js/producto.js
+++ js/producto.js
@@ -1,8 +1,30 @@
-document.addEventListener("DOMContentLoaded",function(){
-    document.addEventListener("submit",function(event){
-        event.preventDefault()
-        let caja=document.getElementById("ficha")
-        let producto=document.getElementById("producto").value
-        caja.innerHTML="El producto seleccionado es "+producto+"."
-        })
-    })
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("submit", function(event) {
+        event.preventDefault();
+        let caja = document.getElementById("ficha");
+        let producto = document.querySelector("input[name='producto']:checked").value.toUpperCase();
+
+
+fetch ("productos.json")
+        .then(respuesta => respuesta.json())
+        .then(datos=>{
+              for (let i in datos){
+                if(datos[i].nombre.toUpperCase() == producto){
+                    ficha.innerHTML=`<strong>El producto seleccionado es ${datos[i].nombre}.</strong><br><br><br>
+                    <strong>FICHA TÉCNICA</strong><br><br>
+                    <strong>Almacenamiento recomendado:</strong> ${datos[i].almacenamiento_recomendado}<br>
+                    <strong>Vida a temperatura ambiente:</strong> ${datos[i].vida_ambiente}<br>
+                    <strong>Vida bajo refrigeración:</strong> ${datos[i].vida_refrigeracion}<br>
+                    <strong>Impacto ambiental del cultivo:</strong> ${datos[i].impacto_ambiental}<br>
+                    <strong>Descripción:</strong> ${datos[i].descripcion}<br>
+                    <strong>Usos recomendados:</strong><br>
+                    <ul>
+                    ${datos[i].usos.map(uso => `<li>${uso}</li>`).join("")}
+                    </ul>
+                    `;
+                }
+            }
+           
+        });
+});
+});
